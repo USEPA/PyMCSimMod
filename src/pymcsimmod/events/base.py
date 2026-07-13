73a8a2bf92a6f8844c6b1018479007ae07f14881
Todd Zurlinden <7946625+tjzurlin@users.noreply.github.com>
2026-07-12 20:42:34 -0400

    Update lock file for dependencies (#15)
    * Update lock file for dependencies
    
    * Ignore unused variable ruff rule (RUF059)

--- src/pymcsimmod/events/base.py
+++ src/pymcsimmod/events/base.py
@@ -111,7 +111,7 @@ class NDoses(BaseEventScheduler):
             if t_start <= t0 <= t_end:
                 val = (
                     self.value[i]
-                    if isinstance(self.value, (list, tuple, np.ndarray))  # noqa: UP038
+                    if isinstance(self.value, (list, tuple, np.ndarray))
                     else self.value
                 )
                 events.append(
